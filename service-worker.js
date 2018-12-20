cb4024a092243c5840a4e71a746de729f312b948
Bargav <bkondapu@veracode.com>
2018-12-19 22:53:59 -0500

    Service worker - cache disabled.

--- service-worker.js
+++ service-worker.js
@@ -5,58 +5,35 @@
  *
  */
 
-const version = "0.0.2";
+const version = "0.0.1";
 const cacheName = `bargavkondapu-${version}`;
-
-self.addEventListener('install', function(evt) {
-  evt.waitUntil(caches.open(cacheName).then(function (cache) {
-  cache.addAll([
-    `/css/w3.css`,
-    `/css/bkondapu.css`,
-    `/`,
-    `https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css`,
-    `/index.html`,
-    `/404.html`,
-    `/page/2/`,
-    `/page/3/`
-  ]);
-}));
-});
-
-self.addEventListener('fetch', function(evt) {
-  evt.respondWith(fromCache(evt.request));
-
-  evt.waitUntil(
-    update(evt.request)
-    .then(refresh)
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/css/w3.css`,
+        `/css/bkondapu.css`,
+        `/`,
+        `/index.html`,
+        `/404.html`,
+        `/page/2/`,
+        `/page/3/`
+      ])
+          .then(() => self.skipWaiting());
+    })
   );
 });
 
-function fromCache(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return cache.match(request);
-  });
-}
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-function update(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
-
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-      var message = {
-        type: 'refresh',
-        url: response.url,
-        eTag: response.headers.get('ETag')
-      };
-      client.postMessage(JSON.stringify(message));
-   });
- });
-}
+/* self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+}); */
